48aeea27aad27fedda19a1555a42b9e72c379f76
Andrew-Mulvaney <111915293+Andrew-Mulvaney@users.noreply.github.com>
2022-09-09 11:54:06 -0400

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Andrew Mulvaneye>
+ *        Date: <September 8th, 2022>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << end1;
+    return 0;
+}// main
